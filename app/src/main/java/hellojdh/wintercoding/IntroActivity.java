49762fbfdc142fb67a4051d0e35272e3087c692d
jdh <jdh940130@gmail.com>
2018-10-30 19:30:13 +0900

    IntroActivity & Gradle update

--- app/src/main/java/hellojdh/wintercoding/IntroActivity.java
+++ app/src/main/java/hellojdh/wintercoding/IntroActivity.java
@@ -1,14 +1,27 @@
 package hellojdh.wintercoding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import hellojdh.wintercoding.tool.BaseActivity;
 
 public class IntroActivity extends BaseActivity {
+    private static int INTRO_TIME = 1300;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },INTRO_TIME);
     }
 }
